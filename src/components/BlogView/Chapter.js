f6d3a8a9416941253e475bf7c78040a6158e47ac
Kurppa <peik.feiring@gmail.com>
2019-06-05 13:10:46 +0300

    line breaks in blogs working

--- src/components/BlogView/Chapter.js
+++ src/components/BlogView/Chapter.js
@@ -3,15 +3,19 @@ import { Header, Divider } from 'semantic-ui-react'
 
 const Chapter = ({ chapter }) => {
 
+  const parts = chapter.content.split('\n')
+
+  console.log(parts.length)
+
   return (
     <div style={{ padding: '1.5em 0', wordWrap: 'break-word' }}>
       <Divider />
 
       <Header as='h3'>{chapter.title}</Header>
       <div className={'large text'}>
-        {
-          chapter.content
-        }
+        {chapter.content.split('\n').map((item, key) => {
+          return <span key={key}>{item}<br/></span>
+        })}
       </div>
     </div>
   )
